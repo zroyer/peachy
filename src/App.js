e551cd22c60428c1be10cdb45a4318bb57ccef8b
Zach Royer <zroyer@utexas.edu>
2017-12-01 15:36:09 -0600

    fetches price

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import LivePrice from './components/LivePrice';
 
 class App extends Component {
   render() {
@@ -8,6 +9,7 @@ class App extends Component {
         <header className="App-header">
           <h1><span role="img" aria-labelledby="peach">🍑</span></h1>
         </header>
+        <LivePrice />
       </div>
     );
   }
---
+++ src/components/LivePrice.js
@@ -0,0 +1,55 @@
+import React from 'react';
+
+class LivePrice extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      price: 0,
+      lastUpdated: null,
+    }
+  }
+
+  componentDidMount() {
+    this.fetchPrice = async () => {
+      const url = 'http://api.coindesk.com/v1/bpi/currentprice.json';
+      fetch(url)
+        .then(response => response.json())
+        .then((priceRes) => {
+          console.log('got the price');
+          console.log(priceRes);
+          this.setState({
+            price: priceRes.bpi.USD.rate_float,
+            lastUpdated: priceRes.time.updated,
+          })
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    }
+
+    this.fetchPrice();
+    this.refresh = setInterval(() => this.fetchPrice(), 90000);
+  }
+
+  componentWillUnmount(){
+    clearInterval(this.refresh);
+  }
+
+  render() {
+    return (
+      <div id="price-container">
+        <div className='price-headline'>
+          {this.state.price.toLocaleString('us-EN', {
+            style: 'currency',
+            currency: 'USD',
+          })}
+        </div>
+        <div className='last-updated-info'>
+          (Last updated {this.state.lastUpdated})
+        </div>
+      </div>
+    )
+  }
+}
+
+export default LivePrice;
