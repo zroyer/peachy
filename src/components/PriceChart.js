77d9826c8d450fa11d689263ae973fd571e8899d
Zach Royer <zroyer@utexas.edu>
2017-12-05 16:03:57 -0600

    chart styling

--- src/components/PriceChart.js
+++ src/components/PriceChart.js
@@ -50,8 +50,8 @@ class PriceChart extends React.Component {
           data={this.state.priceData}>
           <defs>
             <linearGradient id="peachyGradient" x1="0" y1="0" x2="0" y2="1">
-              <stop offset="5%" stopColor="#FFD6BF" stopOpacity={0.8}/>
-              <stop offset="95%" stopColor="#FFD6BF" stopOpacity={0}/>
+              <stop offset="5%" stopColor="#FFD6BF" stopOpacity={0.9}/>
+              <stop offset="95%" stopColor="#FFD6BF" stopOpacity={0.1}/>
             </linearGradient>
           </defs>
           <XAxis
