2a4c671691bd3c7398f1a04f849ed71d084421f9
Zach Royer <zroyer@utexas.edu>
2017-12-07 16:28:22 -0600

    antd

--- src/components/PriceChart.js
+++ src/components/PriceChart.js
@@ -3,6 +3,9 @@ import moment from 'moment';
 import '../style/PriceChart.css';
 import CustomTooltip from './CustomTooltip'
 import {AreaChart, CartesianGrid, Area, XAxis, YAxis, Tooltip} from 'recharts';
+import { Radio } from 'antd';
+const RadioButton = Radio.Button;
+const RadioGroup = Radio.Group;
 
 const baseUrl = 'https://api.coindesk.com/v1/bpi/historical/close.json';
 
@@ -71,16 +74,27 @@ class PriceChart extends React.Component {
 
   render() {
     return (
-      <div className="container">
-        <div className="price-chart-container">
+      <div className="price-chart-container">
+        <div className="timeframe-container">
+          <RadioGroup
+            onChange={this.handleOptionChange}
+            defaultValue={this.state.selectedOption}
+            className="radio-timeframe">
+            <RadioButton value="1month" className="btn-timeframe">1 Month</RadioButton>
+            <RadioButton value="3month" className="btn-timeframe">3 Months</RadioButton>
+            <RadioButton value="1year" className="btn-timeframe">1 Year</RadioButton>
+            <RadioButton value="3years" className="btn-timeframe">3 Years</RadioButton>
+          </RadioGroup>
+        </div>
+        <div className="chart-container">
         	<AreaChart
-            width={900}
+            width={800}
             height={400}
             data={this.state.priceData}>
             <defs>
               <linearGradient id="peachyGradient" x1="0" y1="0" x2="0" y2="1">
-                <stop offset="5%" stopColor="#FFD6BF" stopOpacity={0.9}/>
-                <stop offset="95%" stopColor="#FFD6BF" stopOpacity={0.1}/>
+                <stop offset="5%" stopColor="#ffcba4" stopOpacity={0.9}/>
+                <stop offset="95%" stopColor="#ffcba4" stopOpacity={0.1}/>
               </linearGradient>
             </defs>
             <XAxis
@@ -89,13 +103,13 @@ class PriceChart extends React.Component {
               tick={{ fontSize: 12, dy: 15 }}
               angle={-45}
               height={40}
-              stroke="#808080" />
+              />
             <YAxis
               type="number"
               tickLine={false}
               tick={{ fontSize: 16 }}
               tickFormatter={this.priceFormatter}
-              stroke="#808080" />
+               />
             <CartesianGrid strokeDasharray="2 2" />
             <Tooltip
               content={<CustomTooltip price={this.state.priceData}/>}
@@ -103,50 +117,11 @@ class PriceChart extends React.Component {
             <Area
               type="monotone"
               dataKey="price"
-              stroke="#FFD6BF"
+              stroke="#ffcba4"
               fillOpacity={1}
               fill="url(#peachyGradient)"
               isAnimationActive={false} />
           </AreaChart>
-        </div>
-
-        <div>
-          <div className="radio">
-            <label>
-              <input type="radio"
-                value="1month"
-                checked={this.state.selectedOption === '1month'}
-                onChange={this.handleOptionChange} />
-              1 Month
-            </label>
-          </div>
-          <div className="radio">
-            <label>
-              <input type="radio"
-                value="3month"
-                checked={this.state.selectedOption === '3month'}
-                onChange={this.handleOptionChange} />
-              3 Months
-            </label>
-          </div>
-          <div className="radio">
-            <label>
-              <input type="radio"
-                value="1year"
-                checked={this.state.selectedOption === '1year'}
-                onChange={this.handleOptionChange} />
-              1 Year
-            </label>
-          </div>
-          <div className="radio">
-            <label>
-              <input type="radio"
-                value="3year"
-                checked={this.state.selectedOption === '3year'}
-                onChange={this.handleOptionChange} />
-              3 Years
-            </label>
-          </div>
         </div>
       </div>
     );
