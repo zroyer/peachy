61d81f794147bbce53404b2ecf8f52440f26226c
Zach Royer <zroyer@utexas.edu>
2017-12-11 16:22:38 -0600

    adds RadioButton render test

--- src/tests/PriceChart.test.js
+++ src/tests/PriceChart.test.js
@@ -2,16 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import PriceChart from '../components/PriceChart';
 import { Radio } from 'antd';
-const RadioButton = Radio.Button;
 const RadioGroup = Radio.Group;
+const RadioButton = Radio.Button;
 
 
-it('renders without crashing', () => {
+it('renders PriceChart without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<PriceChart />, div);
 });
 
-it('renders without crashing', () => {
+it('renders RadioGroup without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<RadioGroup />, div);
+});
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<RadioButton />, div);
 });
