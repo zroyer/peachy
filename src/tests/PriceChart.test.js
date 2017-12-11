3f7a86d7eef751fa9f8595372e59518dbe5ad082
Zach Royer <zroyer@utexas.edu>
2017-12-11 16:20:20 -0600

    adds RadioGroup render test

--- src/tests/PriceChart.test.js
+++ src/tests/PriceChart.test.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import PriceChart from '../components/PriceChart';
+import { Radio } from 'antd';
+const RadioButton = Radio.Button;
+const RadioGroup = Radio.Group;
+
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<PriceChart />, div);
+});
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<RadioGroup />, div);
 });
