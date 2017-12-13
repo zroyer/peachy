d0be5a7987b90eee4be2ba4f07de3f5f2156b009
Zach Royer <zroyer@utexas.edu>
2017-12-13 14:08:28 -0600

    adds enzyme and tests

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <h2>Welcome to React</h2>
         <LivePrice />
         <PriceChart />
       </div>
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,8 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
+import sinon from 'sinon';
 import App from '../components/App';
+import LivePrice from '../components/LivePrice';
+import PriceChart from '../components/PriceChart';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+describe('<App />', () => {
+  it('renders a LivePrice component', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(LivePrice).length).toBe(1);
+  });
+});
+
+describe('<App />', () => {
+  it('renders a PriceChart component', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(PriceChart).length).toBe(1);
+  });
 });
