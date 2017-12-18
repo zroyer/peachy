f0bdec296ab95c5ae1d4e37429871a42dea96822
Zach Royer <zroyer@utexas.edu>
2017-12-18 13:32:53 -0600

    refactors tooltip template literalPs

--- src/components/CustomTooltip.js
+++ src/components/CustomTooltip.js
@@ -5,10 +5,11 @@ class CustomTooltip extends React.Component {
   render() {
     const { active, payload, label } = this.props;
     if (active && payload) {
+      let price = `$${payload[0].value.toFixed(2)}`
       return (
         <div className="custom-tooltip">
-          <span className="label-date">{`${label}`}</span>
-          <span className="label-price">${payload[0].value.toFixed(2)}</span>
+          <span className="label-date">{label}</span>
+          <span className="label-price">{price}</span>
         </div>
       );
     }
